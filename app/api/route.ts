d80a0119b2b6b163b06c0d4c99e517e1760359b1
Hardik Jain <jainhardik120@gmail.com>
2024-05-26 21:10:54 +0530

    Update

--- app/api/route.ts
+++ app/api/route.ts
@@ -2,4 +2,7 @@ import { revalidatePath } from "next/cache";
 
 export async function GET(request : Request) {
   revalidatePath("/")
+  return Response.json({
+    message : "Building Path..."
+  })
 }
