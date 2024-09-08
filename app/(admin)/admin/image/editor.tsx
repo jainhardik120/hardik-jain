ef57e56861f210d5862723108479542eb4f19a11
Hardik Jain <jainhardik120@gmail.com>
2024-09-08 16:14:44 +0530

    Fixed SSR error in Image Editor

---
+++ app/(admin)/admin/image/editor.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import Konva from 'konva';
+import React from 'react';
+import { Stage, Layer, Star, Text } from 'react-konva';
+
+const Editor = () => {
+
+  function generateShapes() {
+    return [...Array(10)].map((_, i) => ({
+      id: i.toString(),
+      x: Math.random() * window.innerWidth,
+      y: Math.random() * window.innerHeight,
+      rotation: Math.random() * 180,
+      isDragging: false,
+    }));
+  };
+  const [stars, setStars] = React.useState(generateShapes());
+  const handleDragStart = (e: Konva.KonvaEventObject<DragEvent>) => {
+    const id = e.target.id();
+    setStars(
+      stars.map((star) => {
+        return {
+          ...star,
+          isDragging: star.id === id,
+        };
+      })
+    );
+  };
+  const handleDragEnd = (e: Konva.KonvaEventObject<DragEvent>) => {
+    setStars(
+      stars.map((star) => {
+        return {
+          ...star,
+          isDragging: false,
+        };
+      })
+    );
+  };
+
+  return (
+    <Stage width={window.innerWidth} height={window.innerHeight}>
+      <Layer>
+        <Text text="Try to drag a star" />
+        {stars.map((star) => (
+          <Star
+            key={star.id}
+            id={star.id}
+            x={star.x}
+            y={star.y}
+            numPoints={5}
+            innerRadius={20}
+            outerRadius={40}
+            fill="#89b717"
+            opacity={0.8}
+            draggable
+            rotation={star.rotation}
+            shadowColor="black"
+            shadowBlur={10}
+            shadowOpacity={0.6}
+            onDragStart={handleDragStart}
+            onDragEnd={handleDragEnd}
+          />
+        ))}
+      </Layer>
+    </Stage>
+  )
+}
+
+export default Editor
