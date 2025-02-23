7f39f372d0189edc2ee10c9d7f38fe35e2d0abcc
Hardik Jain <jainhardik120@gmail.com>
2025-02-23 12:48:08 +0530

    Created Prisma Authentication with Email Magic Links
    Created method for server side authentication check
    
    Added TRPc Setup

--- app/(admin)/admin/(authorised)/image/page.tsx
+++
@@ -1,7 +0,0 @@
-import dynamic from 'next/dynamic';
-
-const Editor = dynamic(() => import('./editor'), { ssr: false });
-
-export default function Page() {
-  return <Editor />;
-}
--- lib/ErrorResponse.ts
+++
@@ -1,3 +0,0 @@
-export default function ErrorResponse(error : any, status? : number){
-  return Response.json({error : error.message || error.toString() || "Unknown Error Occured"}, {status : status || 500})
-} 
