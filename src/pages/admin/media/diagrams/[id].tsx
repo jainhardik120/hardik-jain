75a3410832990a330c6b4cd4d19a0cb0b3fa70af
Hardik Jain <jainhardik120@gmail.com>
2025-03-02 23:40:53 +0530

    Updated excalidraw messages

--- src/pages/admin/media/diagrams/[id].tsx
+++ src/pages/admin/media/diagrams/[id].tsx
@@ -7,14 +7,9 @@ import { importExcalidraw } from '@/lib/excalidraw';
 import { SidebarLayout } from '@/components/sidebar/sidebar-layout';
 import { Skeleton } from '@/components/ui/skeleton';
 
-const ExcalidrawWrapper = dynamic(
-  async () => (await import('@/components/excalidraw/ExcalidrawWrapper')).default,
-  {
-    ssr: false,
-  },
-);
-
-export async function getServerSideProps(context: { params: { id: string } }) {
+export async function getServerSideProps(context: {
+  params: { id: string };
+}): Promise<{ props: { id: string } }> {
   const { id } = context.params;
 
   return {
@@ -24,7 +19,7 @@ export async function getServerSideProps(context: { params: { id: string } }) {
   };
 }
 
-export default function Page({ id }: { id: string }) {
+export default function Page({ id }: { id: string }): JSX.Element {
   const signedUrl = trpc.excalidraw.getSignedUrlDesign.useQuery({ id }, { enabled: false });
   const session = trpc.auth.sessionDetails.useQuery().data || null;
   const [initialExcalidrawData, setInitialExcalidrawData] = useState<
@@ -32,25 +27,46 @@ export default function Page({ id }: { id: string }) {
   >(undefined);
   const [isLoaded, setIsLoaded] = useState(false);
   const [error, setError] = useState<string | undefined>(undefined);
+  const [message, setMessage] = useState('');
   useEffect(() => {
-    signedUrl.refetch();
+    const fetchData = async (): Promise<void> => {
+      setMessage('Fetching data url...');
+      await signedUrl.refetch();
+      setMessage('');
+    };
+    void fetchData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
   useEffect(() => {
     if (!signedUrl.data) {
       return;
     }
+    setMessage('Downloading data...');
     importExcalidraw(signedUrl.data.elementsUrl, signedUrl.data.filesUrl)
       .then((data) => {
         setInitialExcalidrawData(data);
       })
       .catch((error) => {
-        setError(`Error importing and updating scene: ${error}`);
+        setError(`Error downloading data, ${error}`);
       })
       .finally(() => {
+        setMessage('');
         setIsLoaded(true);
       });
   }, [signedUrl.data]);
+
+  const ExcalidrawWrapper = dynamic(
+    async () => {
+      setMessage('Downloading Excalidraw editor...');
+      const excalidrawModule = await import('@/components/excalidraw/ExcalidrawWrapper');
+      setMessage('');
+      return excalidrawModule.default;
+    },
+    {
+      ssr: false,
+      loading: () => <Skeleton className="w-full h-full" />,
+    },
+  );
 
   return (
     <SidebarLayout
@@ -60,19 +76,27 @@ export default function Page({ id }: { id: string }) {
         email: session?.user.email ?? '',
         avatar:
           session?.user.image ??
+          // eslint-disable-next-line max-len
           `https://api.dicebear.com/9.x/thumbs/svg?seed=${Math.floor(Math.random() * 100000) + 1}&randomizeIds=true`,
       }}
     >
       <Header>
         <div className="flex w-full flex-row justify-between items-center">
           <p>Excalidraw Diagram</p>
-          <p>End title</p>
+          <p>{message}</p>
         </div>
       </Header>
-      {error !== undefined && <div>{error}</div>}
-      {!isLoaded && <Skeleton className="w-full h-full" />}
-      {isLoaded && initialExcalidrawData && (
-        <ExcalidrawWrapper initialData={initialExcalidrawData} id={id} />
+      {error !== undefined ? (
+        <div>{error}</div>
+      ) : !(isLoaded && initialExcalidrawData !== undefined) ? (
+        <Skeleton className="w-full h-full" />
+      ) : (
+        <ExcalidrawWrapper
+          initialData={initialExcalidrawData}
+          id={id}
+          setMessage={setMessage}
+          setError={setError}
+        />
       )}
     </SidebarLayout>
   );
