17216ec6f7338c92de8750b65d65036fc631ce1d
Hardik Jain <jainhardik120@gmail.com>
2025-03-03 18:05:42 +0530

    Created Refresh and Access Tokens

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -90,7 +90,7 @@ function LoginForm(): JSX.Element {
     if (isValid) {
       setError('');
       startTransition(() => {
-        signIn('email', {
+        void signIn('email', {
           email: form.getValues('email'),
           redirectTo: callbackUrl,
         });
