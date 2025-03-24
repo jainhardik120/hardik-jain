f2d8ba9870088d90a1a50121ca2e11eb5c5b51f8
Hardik Jain <jainhardik120@gmail.com>
2025-03-24 19:36:11 +0530

    refactor: :rocket: Created Form component

--- src/components/email-editor/panels/Tabs/DesktopPreview.tsx
+++ src/components/email-editor/panels/Tabs/DesktopPreview.tsx
@@ -5,7 +5,7 @@ const DesktopPreview = () => {
   const { html } = useLayout();
   return (
     <div className="w-full h-full border rounded-md bg-muted/20 flex items-center justify-center">
-      <iframe srcDoc={html} className="h-full w-full border-none" />
+      <iframe title="Email Desktop Preview" srcDoc={html} className="h-full w-full border-none" />
     </div>
   );
 };
