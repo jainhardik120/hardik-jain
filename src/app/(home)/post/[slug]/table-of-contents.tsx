b9b6ab41774b827a4b485a393aa690dee2c8381d
Hardik Jain <jainhardik120@gmail.com>
2025-04-05 15:58:12 +0530

    feat: Add account form for user details update and implement account details schema

--- src/app/(home)/post/[slug]/table-of-contents.tsx
+++ src/app/(home)/post/[slug]/table-of-contents.tsx
@@ -70,7 +70,7 @@ export default function TableOfContents() {
   }
 
   return (
-    <div className="mb-8">
+    <div>
       <h2 className="text-lg font-semibold mb-4">Table of Contents</h2>
       <nav>
         <ul className="space-y-2 text-sm">
--- src/app/admin/(normal)/account/page.tsx
+++ src/app/admin/(normal)/account/page.tsx
@@ -1,9 +1,28 @@
+import { notFound } from 'next/navigation';
+
+import { api } from '@/server/api/server';
+
+import AccountForm from './account-form';
 import CanvaConnection from './canva-connection';
 
-export default function AccountPage() {
+export default async function AccountPage() {
+  const userDetails = await api.user.getUserDetails();
+  if (!userDetails) {
+    return notFound();
+  }
   return (
     <div className="flex flex-col h-full w-full">
       <h2>Account Page</h2>
+      <AccountForm
+        userDetails={{
+          ...userDetails,
+          name: userDetails.name ?? '',
+          image: userDetails.image ?? '',
+          twitter: userDetails.twitter ?? '',
+          linkedin: userDetails.linkedin ?? '',
+          website: userDetails.website ?? '',
+        }}
+      />
       <CanvaConnection />
     </div>
   );
